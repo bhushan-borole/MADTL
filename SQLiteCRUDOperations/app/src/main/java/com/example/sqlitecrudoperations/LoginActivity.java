cddddf913f848e63718a0aa29f98218d58889d00
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2019-09-30 20:20:12 +0530

    Added CDMA and GSM

--- SQLiteCRUDOperations/app/src/main/java/com/example/sqlitecrudoperations/LoginActivity.java
+++ SQLiteCRUDOperations/app/src/main/java/com/example/sqlitecrudoperations/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.sqlitecrudoperations;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
     Button btn_submit, canvas, location_btn, notification;
     EditText login, password;
     TextView register;
+    Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
