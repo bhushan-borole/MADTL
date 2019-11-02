63028ec7b60a36d3dee3e4e05bc05568f4206a67
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2019-11-02 21:57:32 +0530

    Added Chit 5

---
+++ Chits/J2ME/src/com/chit5/Reverse.java
@@ -0,0 +1,64 @@
+package com.chit5;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.StringItem;
+import javax.microedition.lcdui.TextField;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+public class Reverse extends MIDlet implements CommandListener{
+	
+	private Form form;
+	private Display display;
+	private TextField inp;
+	private StringItem op;
+	private Command com;
+
+	public Reverse() {
+		inp = new TextField("Enter string:", "", 40, TextField.ANY);
+		com = new Command("OK", Command.OK, 2);
+	}
+
+	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
+		notifyDestroyed();
+	}
+
+	protected void pauseApp() {}
+
+	protected void startApp() throws MIDletStateChangeException {
+		display = Display.getDisplay(this);
+		form = new Form("Reverse");
+		form.append(inp);
+		form.addCommand(com);
+		form.setCommandListener(this);
+		display.setCurrent(form);
+	}
+	
+	public void show() {
+		display = Display.getDisplay(this);
+		String input = inp.getString();
+		String rev = "";
+		for(int i=input.length()-1; i>=0; i--)
+			rev += input.charAt(i);
+		op = new StringItem("Reverse: ", rev);
+		form.append(op);
+		display.setCurrent(form);
+		
+	}
+
+	public void commandAction(Command c, Displayable arg1) {
+		String label = 	c.getLabel();
+		if(label.equals("OK")) {
+			try {
+				form.delete(1);
+			}
+			catch(Exception e) {}
+			show();
+		}
+	}
+
+}
