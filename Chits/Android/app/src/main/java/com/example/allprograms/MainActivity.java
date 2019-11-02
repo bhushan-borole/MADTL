aeba96c4cb5d9e915aae625fad6aecd46fcee3c9
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2019-11-02 21:02:33 +0530

    Added chit 5

--- Chits/Android/app/src/main/java/com/example/allprograms/MainActivity.java
+++ Chits/Android/app/src/main/java/com/example/allprograms/MainActivity.java
@@ -78,5 +78,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        reverse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Reverse.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Chits/Android/app/src/main/java/com/example/allprograms/Reverse.java
@@ -0,0 +1,35 @@
+package com.example.allprograms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Reverse extends AppCompatActivity {
+
+    private Button submit;
+    private EditText input;
+    private TextView op;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reverse);
+
+        submit = findViewById(R.id.submit_rev);
+        input = findViewById(R.id.inp_rev);
+        op = findViewById(R.id.op_rev);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String in = input.getText().toString().trim();
+                StringBuilder sb = new StringBuilder(in);
+                String rev = sb.reverse().toString();
+                op.setText(rev);
+            }
+        });
+    }
+}
